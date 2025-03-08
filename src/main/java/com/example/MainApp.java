ef02a8e92476da81e3b44f6ec83734124be99a60
Jack Rosenberg <jdrosenberg01@bvsd.org>
2025-03-08 15:21:09 -0700

    inital ~ fix VSCode errors

---
+++ src/main/java/com/example/MainApp.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainApp extends javafx.application.Application {
+    //@Override
+    public void start(javafx.stage.Stage primaryStage) {
+        javafx.scene.control.Label label = new javafx.scene.control.Label("Hello, JavaFX!");
+        javafx.scene.layout.StackPane root = new javafx.scene.layout.StackPane(label);
+        javafx.scene.Scene scene = new javafx.scene.Scene(root, 400, 300);
+        
+        primaryStage.setTitle("JavaFX App");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.example {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    
+    exports com.example;
+}
